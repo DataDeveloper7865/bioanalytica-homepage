86dc2cf6b1b89779d3a6adc40c5253b9472a55d3
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-09 15:48:46 -0700

    deleted icon, changed css

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,37 +3,28 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
+  toolbar: {
+  }
 }));
 
-function Header() {
+function NavBar() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
+        <Toolbar className={classes.toolbar}>
           <Typography variant="h6" className={classes.title}>
             Home
           </Typography>
@@ -43,11 +34,10 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             Contact
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
 
-export default Header;
+export default NavBar;
