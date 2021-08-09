607b9b845fd1b0d1dc48b9b2f83d6482b08c9109
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-09 15:49:02 -0700

    added about

---
+++ src/components/About.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
+import ContentPlaceHolder from '../content/ContentPlaceHolder';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        minHeight: '500px'
+    },
+    content: {
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary
+    },
+    title: {
+        flexGrow: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+}))
+
+function About() {
+
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root} >
+            <Grid container className={classes.content}>
+                <Grid item xs={12} md={12}>
+                    <Paper className={classes.title}>
+                    <Typography variant="h5" className={classes.title}>
+                        About
+                    </Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper className={classes.paper} >
+                        <ContentPlaceHolder />
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper className={classes.paper} >
+                        <ContentPlaceHolder />
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper className={classes.paper} >
+                        <ContentPlaceHolder />
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    )
+
+}
+
+export default About;
