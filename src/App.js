7c87f2193885514b7512cdfce0e532f512af1a37
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-09 16:06:19 -0700

    added footer in app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import NavBar from "./NavBar.js";
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Grid>
       <NavBar />
       <About />
+      <Footer />
     </Grid>
   );
 }
