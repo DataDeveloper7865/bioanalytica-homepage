4e942188a1ac94f58fa3ae58c970334d1dce5202
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-09 17:27:14 -0700

    added home component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Grid from "@material-ui/core/Grid";
 import NavBar from "./NavBar.js";
 import About from './components/About';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <Grid>
       <NavBar />
+      <Home />
       <About />
       <Footer />
     </Grid>
