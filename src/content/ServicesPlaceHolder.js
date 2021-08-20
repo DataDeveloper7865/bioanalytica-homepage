769bdda8e7ce45ae7772e447340bfc97b9259168
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-19 18:44:45 -0700

    added placeholder text for services component

---
+++ src/content/ServicesPlaceHolder.js
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+function ServicesPlaceHolder() {
+
+    return (
+        <Typography>
+            Need help on your next project?
+        </Typography>
+    )
+}
+
+export default ServicesPlaceHolder;
